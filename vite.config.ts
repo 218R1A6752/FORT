5cea9a99579fb4f377e26c0080b54ab065846418
VASHISTA RAMAN <218r1a6752@gmail.com>
2025-01-21 23:21:02 +0530

    MESSAGE

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
- base: '/FORT/', // Add this line
+  base: '/FORT/', // Replace with the subdirectory of your hosted app
 });
